e2250faaebfb77d44aa155c651cc2444d3abc49a
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-06-02 21:11:58 +1000

    add eventlistener

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log("hello")
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let selectedWord;
+let userGuesses;
+let wrongGuesses;
+let wrongLocGuesses;
+let rightGuesses;
+/*------------------------ Cached Element References ------------------------*/
+
+const keysElements = document.querySelectorAll(".keyboard-box");
+const lettersElements = document.querySelectorAll(".box");
+
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+  selectedWord = "house";
+  userGuesses = [];
+  wrongGuesses = [];
+  wrongLocGuesses = [];
+  rightGuesses = [];
+};
+const onKeyboardClicked = (event) => {
+  console.log("er are in onKeyboardClick function " + event.target.innerHTML);
+};
+/*----------------------------- Event Listeners -----------------------------*/
+
+keysElements.forEach((element) => {
+  element.addEventListener("click", onKeyboardClicked);
+});
